469ce18517fc1849ec0ed5efdf01c03b230bb052
Smit Solanki <ssolanki@horizontal.com>
2024-09-25 16:33:34 +0530

    orders-api-done

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -27,21 +27,36 @@ exports.getSingleOrder = async (req, res) => {
 };
 
 exports.createOrder = async (req, res) => {
-  const orderItemsIds = Promise.all(
-    req.body.orderItems.map(async (orderItem) => {
-      let newOrderItem = new OrderItem({
-        quantity: orderItem.quantity,
-        product: orderItem.product,
-      });
-
-      newOrderItem = await newOrderItem.save();
-
-      return newOrderItem._id;
-    })
-  );
-
-  const orderItemsIdsResolved = await orderItemsIds;
   try {
+    const orderItemsIds = Promise.all(
+      req.body?.orderItems?.map(async (orderItem) => {
+        let newOrderItem = new OrderItem({
+          quantity: orderItem.quantity,
+          product: orderItem.product,
+        });
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+      })
+    );
+
+    const orderItemsIdsResolved = await orderItemsIds;
+
+    const totalPrices = await Promise.all(
+      orderItemsIdsResolved?.map(async (orderItemId) => {
+        const orderItem = await OrderItem.findById(orderItemId).populate(
+          "product",
+          "price"
+        );
+
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+
+        return totalPrice;
+      })
+    );
+
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
     const order = new Order({
       orderItems: orderItemsIdsResolved,
       user: req.body.user,
@@ -52,7 +67,7 @@ exports.createOrder = async (req, res) => {
       country: req.body.country,
       phone: req.body.phone,
       status: req.body.status,
-      totalPrice: req.body.totalPrice,
+      totalPrice: totalPrice,
       dateOrdered: req.body.dateOrdered,
     });
     const savedOrder = await order.save();
@@ -63,4 +78,82 @@ exports.createOrder = async (req, res) => {
   }
 };
 
-// Add more order-related controller functions here as needed
+exports.updateOrder = async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        status: req.body.status,
+      },
+      { new: true }
+    );
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+    res.json(order);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found!" });
+    }
+
+    // Delete associated order items in parallel
+    await Promise.all(
+      order?.orderItems?.map(async (orderItem) => {
+        await OrderItem.findByIdAndDelete(orderItem?._id);
+      })
+    );
+
+    return res.json({
+      success: true,
+      message: `The order is deleted!`,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+exports.getTotalSales = async (req, res) => {
+  const totalSales = await Order.aggregate([
+    { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
+  ]);
+
+  if (!totalSales) {
+    return res.status(400).send("The order sales cannot be generated");
+  }
+
+  res.send({ totalSales: totalSales.pop().totalsales });
+};
+
+exports.getOrderCount = async (req, res) => {
+  try {
+    const orderCount = await Order.countDocuments();
+    res.json({ orderCount });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+exports.getUserOrder = async (req, res) => {
+  try {
+    const userOrdersList = await Order.find({ user: req.params.userid })
+      .populate({
+        path: "orderItems",
+        populate: { path: "product", populate: "category" },
+      })
+      .sort({ dateOrdered: -1 });
+    res.json(userOrdersList);
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
--- backend/src/routers/orders.js
+++ backend/src/routers/orders.js
@@ -5,6 +5,11 @@ const orderController = require("../controllers/orderController");
 router.get(`/`, orderController.getAllOrders);
 router.get(`/:id`, orderController.getSingleOrder);
 router.post(`/`, orderController.createOrder);
+router.put(`/:id`, orderController.updateOrder);
+router.delete(`/:id`, orderController.deleteOrder);
+router.get(`/get/totalsales`, orderController.getTotalSales);
+router.get(`/get/ordercount`, orderController.getOrderCount);
+router.get(`/get/userorders/:userid`, orderController.getUserOrder);
 
 // Add more order-related routes here as needed
 
